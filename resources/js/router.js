ea078fcd75155a312028efc82dda6c15990386d7
Bartosz Trusinski <bartosztrusinski@gmail.com>
2023-08-31 00:00:25 +0200

    add vue router guard to protect routes

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,11 +1,33 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from "./pages/Dashboard.vue";
+import Login from "./pages/Login.vue";
+import apiClient from "./apiClient";
+import { getStoredUser, setStoredUser } from "./userStorage";
 
-const routes = [{ path: "/", component: Dashboard }];
+const routes = [
+  { path: "/", component: Dashboard },
+  { path: "/login", component: Login },
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
+});
+
+router.beforeEach(async (to, from, next) => {
+  try {
+    const loggedInUser = getStoredUser();
+
+    if (to.path !== "/login" && !loggedInUser) {
+      const { data: user } = await apiClient.get("/user");
+      setStoredUser(user);
+      return next();
+    }
+  } catch (error) {
+    return next("/login");
+  }
+
+  return next();
 });
 
 export default router;

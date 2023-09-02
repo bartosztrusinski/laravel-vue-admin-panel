22bbd19f8ac5c4a9da302b1e186fc7efbe6808a5
Bartosz Trusinski <bartosztrusinski@gmail.com>
2023-09-02 20:31:43 +0200

    create component to list errors

---
+++ resources/js/components/ErrorList.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul
+    class="text-red-400 text-sm"
+    v-for="(value, index) in errors"
+    :key="index"
+    v-if="typeof errors === 'object'"
+  >
+    <li class="list-none">{{ value[0] }}</li>
+  </ul>
+  <p class="text-red-400 text-sm" v-if="typeof errors === 'string'">
+    {{ errors }}
+  </p>
+</template>
+<script setup>
+const props = defineProps(["errors"]);
+</script>

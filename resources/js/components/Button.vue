9f1ee0a59d7b12de5bf09c45cf08eb3aa9e84ee2
Bartosz Trusinski <bartosztrusinski@gmail.com>
2023-09-03 22:16:29 +0200

    add button transition

--- resources/js/components/Button.vue
+++ resources/js/components/Button.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="rounded text-white p-2 font-semibold cursor-pointer"
+    class="rounded text-white p-2 font-semibold cursor-pointer transition-all duration-75"
     type="button"
   >
     <font-awesome-icon v-if="icon" :icon="icon" /> <slot />

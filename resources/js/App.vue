c26fa4ade4e6deb3997914346c7986e4381cadbf
Bartosz Trusinski <bartosztrusinski@gmail.com>
2023-09-03 18:20:07 +0200

    provide menu open ref

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,3 +1,8 @@
 <template>
-    <router-view></router-view>
+  <router-view></router-view>
 </template>
+<script setup>
+import { ref, provide } from "vue";
+const isMenuOpen = ref(false);
+provide("isMenuOpen", isMenuOpen);
+</script>

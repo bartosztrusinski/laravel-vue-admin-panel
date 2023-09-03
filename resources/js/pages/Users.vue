339cf1b12b1b3273ef4d4a854a71e891d021959c
Bartosz Trusinski <bartosztrusinski@gmail.com>
2023-09-03 20:09:18 +0200

    create responsive layout for users

--- resources/js/pages/Users.vue
+++ resources/js/pages/Users.vue
@@ -1,101 +1,118 @@
 <template>
-  <div class="bg-gray-100 min-h-screen">
-    <h1 class="text-4xl font-semibold p-2">Users</h1>
+  <Layout>
+    <h1 class="text-4xl font-semibold px-5 py-3 text-center sm:text-start">
+      <font-awesome-icon icon="fa-solid fa-users" />
+      Users
+    </h1>
 
-    <h2 class="text-xl text-center">Create User</h2>
     <form
       @submit.prevent="handleCreate"
-      class="flex flex-col gap-1 flex-grow max-w-[20rem] m-auto shadow-xl rounded px-6 py-3 bg-white"
+      class="flex flex-col gap-1 shadow-sm bg-white rounded-lg sm:mx-5 p-5 lg:w-[20rem] lg:fixed lg:top-[17rem] lg:left-4 border-2 border-gray-200 my-4 lg:m-0 lg:border-none"
     >
+      <h2 class="text-xl text-center font-semibold">Add new user</h2>
       <label v-for="(_, key) in createForm">
-        <div class="first-letter:uppercase font-light text-lg">
+        <div class="first-letter:uppercase">
           {{ key }}
         </div>
         <input
           v-if="key !== 'role'"
           :type="['email', 'password'].includes(key) ? key : 'text'"
+          :placeholder="`Enter ${key}`"
           v-model="createForm[key]"
-          class="w-full p-1 outline-none border-2 border-gray-300 rounded focus-visible:outline-none focus-visible:border-blue-400"
+          class="w-full p-1 outline-none border-2 border-gray-200 rounded focus-visible:outline-none focus-visible:border-blue-400"
         />
         <select
           v-else
           v-model="createForm[key]"
-          class="w-full p-1 outline-none border-2 border-gray-300 rounded focus-visible:outline-none focus-visible:border-blue-400"
+          class="w-full p-1 outline-none border-2 border-gray-200 rounded focus-visible:outline-none focus-visible:border-blue-400"
         >
+          <option value="" disabled selected>Select role</option>
           <option value="user">User</option>
           <option value="admin">Admin</option>
           <option value="content_editor">Content Editor</option>
         </select>
       </label>
 
-      <ErrorList :errors="errors" />
+      <ErrorList :errors="createErrors" />
 
       <button
         type="submit"
-        class="bg-blue-400 rounded text-white px-2 py-1 my-3 place-self-end font-semibold"
+        class="bg-emerald-500 rounded text-white p-2 mt-2 place-self-end font-semibold"
       >
+        <font-awesome-icon icon="fa-solid fa-plus" />
         Create
       </button>
     </form>
 
     <ul
       v-if="users.length > 0"
-      class="grid grid-cols-1 p-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-sm m-auto sm:max-w-none"
+      class="grid grid-cols-1 xl:grid-cols-2 gap-3 sm:p-5"
     >
       <li v-for="user in users" :key="user.id">
         <div
           v-if="user.id !== originalUserId"
-          class="flex flex-col p-5 rounded-lg m-2 shadow-lg bg-white text-center"
+          class="p-3 rounded-lg shadow-sm border-2 border-solid border-gray-200 bg-white break-words"
         >
-          <h2 class="text-lg">{{ user.name }} {{ user.surname }}</h2>
-          <div>{{ user.email }}</div>
-          <div class="text-teal-500 font-bold uppercase">
-            {{ user.role.replace("_", " ") }}
-          </div>
-          <div class="flex gap-1 justify-end mt-3">
-            <button
-              @click="handleEdit(user.id)"
-              class="px-2 py-1 bg-emerald-500 text-white rounded"
+          <h2 class="text-lg font-medium mb-1">
+            <div class="inline rounded-lg px-2 py-1 border-gray-300 border-2">
+              <font-awesome-icon icon="fa-solid fa-user" />
+            </div>
+            {{ user.name }} {{ user.surname }}
+          </h2>
+          <a :href="`mailto:${user.email}`" class="text-gray-600">{{
+            user.email
+          }}</a>
+
+          <div
+            class="flex flex-col sm:flex-row gap-2 justify-between items-end mt-2"
+          >
+            <div
+              class="text-teal-400 font-semibold m-1 text-sm uppercase border-b-2 border-teal-400"
             >
-              Edit
-            </button>
-            <button
-              @click="handleDelete(user.id)"
-              class="px-2 py-1 bg-red-400 text-white rounded"
-            >
-              Delete
-            </button>
+              {{ user.role.replace("_", " ") }}
+            </div>
+
+            <div>
+              <button
+                @click="handleEdit(user.id)"
+                class="p-2 mr-1 bg-amber-500 text-white rounded font-semibold"
+              >
+                <font-awesome-icon icon="fa-solid fa-pencil" />
+                Edit
+              </button>
+              <button
+                @click="handleDelete(user.id)"
+                class="p-2 bg-red-400 text-white rounded font-semibold"
+              >
+                <font-awesome-icon icon="fa-solid fa-trash-can" />
+                Delete
+              </button>
+            </div>
           </div>
         </div>
         <div
           v-else
-          class="flex flex-col p-5 rounded-lg m-2 shadow-lg bg-white text-center"
+          class="p-3 rounded-lg shadow-sm border-2 border-solid border-gray-200 bg-white"
         >
           <form
             @submit.prevent="handleUpdate(user.id)"
-            class="flex flex-col gap-1 flex-grow max-w-[20rem] m-auto"
+            class="flex flex-col gap-1"
           >
-            <div class="text-red-500">
-              <div v-for="error in errors" :key="error">
-                <div v-for="message in error" :key="message">
-                  {{ message }}
-                </div>
-              </div>
-            </div>
             <label v-for="(_, key) in editForm">
-              <div class="first-letter:uppercase font-light text-lg">
+              <div class="first-letter:uppercase">
                 {{ key }}
               </div>
               <input
                 v-if="key !== 'role'"
                 :type="key === 'email' ? key : 'text'"
                 v-model="editForm[key]"
-                class="w-full p-1 outline-none border-2 border-gray-300 rounded focus-visible:outline-none focus-visible:border-blue-400"
+                :placeholder="`Enter ${key}`"
+                class="w-full p-1 outline-none border-2 border-gray-200 rounded focus-visible:outline-none focus-visible:border-blue-400"
               />
               <select
                 v-else
                 v-model="editForm[key]"
-                class="w-full p-1 outline-none border-2 border-gray-300 rounded focus-visible:outline-none focus-visible:border-blue-400"
+                class="w-full p-1 outline-none border-2 border-gray-200 rounded focus-visible:outline-none focus-visible:border-blue-400"
               >
                 <option value="user">User</option>
                 <option value="admin">Admin</option>
@@ -103,18 +120,22 @@
               </select>
             </label>
 
-            <div class="place-self-end mt-3">
+            <ErrorList :errors="editErrors" />
+
+            <div class="place-self-end mt-2">
               <button
                 type="submit"
-                class="bg-emerald-500 rounded text-white px-2 py-1 mr-1 font-semibold"
+                class="bg-emerald-500 rounded text-white p-2 mr-1 font-semibold"
               >
+                <font-awesome-icon icon="fa-solid fa-check" />
                 Update
               </button>
               <button
                 type="button"
                 @click="handleCancel"
-                class="bg-red-400 rounded text-white px-2 py-1 font-semibold"
+                class="bg-red-400 rounded text-white p-2 font-semibold"
               >
+                <font-awesome-icon icon="fa-solid fa-xmark" />
                 Cancel
               </button>
             </div>
@@ -122,15 +143,16 @@
         </div>
       </li>
     </ul>
-  </div>
+  </Layout>
 </template>
 <script setup>
 import { ref } from "vue";
 import apiClient from "../apiClient";
 import ErrorList from "../components/ErrorList.vue";
+import Layout from "../components/Layout.vue";
 
 const users = ref([]);
-const errors = ref({});
+const createErrors = ref();
 const createForm = ref({
   email: "",
   name: "",
@@ -139,6 +161,7 @@ const createForm = ref({
   role: "",
 });
 
+const editErrors = ref();
 const editForm = ref({
   email: "",
   name: "",
@@ -176,9 +199,9 @@ const handleCreate = async () => {
       password: "",
       role: "",
     };
-    errors.value = {};
+    createErrors.value = {};
   } catch (error) {
-    errors.value = error.response.data.errors;
+    createErrors.value = error.response.data.errors;
   }
 };
 
@@ -199,10 +222,10 @@ const handleUpdate = async (userId) => {
       surname: "",
       role: "",
     };
-    errors.value = {};
+    createErrors.value = {};
     originalUserId.value = null;
   } catch (error) {
-    errors.value = error.response.data.errors;
+    createErrors.value = error.response.data.errors;
   }
 };
 
@@ -211,7 +234,7 @@ const handleDelete = async (userId) => {
     await apiClient.delete(`/users/${userId}`);
     users.value = users.value.filter((user) => user.id !== userId);
   } catch (error) {
-    errors.value = error.response.data.errors;
+    createErrors.value = error.response.data.errors;
   }
 };
 

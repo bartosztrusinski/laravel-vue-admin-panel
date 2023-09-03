d2c4288d390d76a20f60e7c5ae959494897a52a2
Bartosz Trusinski <bartosztrusinski@gmail.com>
2023-09-03 22:11:23 +0200

    create button component

---
+++ resources/js/components/Button.vue
@@ -0,0 +1,13 @@
+<template>
+  <button
+    class="rounded text-white p-2 font-semibold cursor-pointer"
+    type="button"
+  >
+    <font-awesome-icon v-if="icon" :icon="icon" /> <slot />
+  </button>
+</template>
+<script setup>
+import { defineProps } from "vue";
+
+const { icon } = defineProps(["icon"]);
+</script>

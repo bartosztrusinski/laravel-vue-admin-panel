4c6425b31e317bb17c562c41798446c458dfd072
Bartosz Trusinski <bartosztrusinski@gmail.com>
2023-09-03 22:41:12 +0200

    apply focus styles

--- resources/js/components/Button.vue
+++ resources/js/components/Button.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="rounded text-white p-2 font-semibold cursor-pointer transition-all duration-75"
+    class="appearance-none rounded text-white p-2 font-semibold cursor-pointer transition-all duration-75 focus-visible:outline-none focus-visible:ring-2 focus-visible:border-transparent"
     type="button"
   >
     <font-awesome-icon v-if="icon" :icon="icon" /> <slot />

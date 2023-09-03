2a1f7d2a9f08d4121e3e9ed190bb4b40484c6922
Bartosz Trusinski <bartosztrusinski@gmail.com>
2023-09-03 22:41:29 +0200

    add hover styles and icons

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,23 +1,31 @@
 <template>
   <Layout>
-    <h1 class="text-3xl sm:text-4xl md:text-5xl font-bold p-6">
+    <h1
+      class="text-3xl sm:text-4xl md:text-5xl font-bold p-6 text-center sm:text-start"
+    >
       Welcome {{ user.name + " " + user.surname }}!
     </h1>
-    <div class="flex flex-col sm:flex-row gap-5 p-5">
+    <div class="flex flex-col sm:flex-row gap-10 p-5">
       <RouterLink
         to="/users"
-        class="border-gray-200 border-2 border-solid rounded-lg p-12 basis-full sm:basis-1/2 hover:bg-gray-100"
+        class="border-gray-200 border-2 border-solid rounded-lg p-8 sm:p-12 basis-full sm:basis-1/2 hover:bg-gray-100 hover:scale-105 transition-all duration-100"
       >
-        <h2 class="font-semibold text-2xl">Users</h2>
+        <h2 class="font-semibold text-2xl">
+          <font-awesome-icon icon="fa-solid fa-users" />
+          Users
+        </h2>
         <p class="text-gray-400 text-lg">
           Click here to see, manage, create and delete users
         </p>
       </RouterLink>
       <RouterLink
         to="/posts"
-        class="border-gray-200 border-2 border-solid rounded-lg p-12 basis-full sm:basis-1/2 hover:bg-gray-100"
+        class="border-gray-200 border-2 border-solid rounded-lg p-8 sm:p-12 basis-full sm:basis-1/2 hover:bg-gray-100 hover:scale-105 transition-all duration-100"
       >
-        <h2 class="font-semibold text-2xl">Posts</h2>
+        <h2 class="font-semibold text-2xl">
+          <font-awesome-icon icon="fa-solid fa-message" />
+          Posts
+        </h2>
         <p class="text-gray-400 text-lg">
           Click here to see, manage, create and delete posts
         </p>

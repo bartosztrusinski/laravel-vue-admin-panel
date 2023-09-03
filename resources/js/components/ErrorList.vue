210adaa55c10fbf9e8ac950ddcc80c0ac1eb47f3
Bartosz Trusinski <bartosztrusinski@gmail.com>
2023-09-03 18:21:42 +0200

    render only if errors exist

--- resources/js/components/ErrorList.vue
+++ resources/js/components/ErrorList.vue
@@ -1,13 +1,10 @@
 <template>
-  <ul
-    class="text-red-400 text-sm"
-    v-for="(value, index) in errors"
-    :key="index"
-    v-if="typeof errors === 'object'"
-  >
-    <li class="list-none">{{ value[0] }}</li>
+  <ul class="text-red-400 text-sm" v-if="errors && typeof errors === 'object'">
+    <li v-for="(value, index) in errors" :key="index" class="list-none">
+      {{ value[0] }}
+    </li>
   </ul>
-  <p class="text-red-400 text-sm" v-if="typeof errors === 'string'">
+  <p class="text-red-400 text-sm" v-if="errors && typeof errors === 'string'">
     {{ errors }}
   </p>
 </template>

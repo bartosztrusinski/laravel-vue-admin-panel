337e3c43ceed51bec62ca7c0aa641fab2a815f55
Bartosz Trusinski <bartosztrusinski@gmail.com>
2023-09-03 18:19:27 +0200

    setup font awesome component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,40 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faBars,
+  faHome,
+  faMessage,
+  faPencil,
+  faPlus,
+  faTrashCan,
+  faUser,
+  faUsers,
+  faXmark,
+  faArrowRightFromBracket,
+  faShieldHalved,
+  faCheck,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(
+  faUser,
+  faPlus,
+  faMessage,
+  faHome,
+  faTrashCan,
+  faPencil,
+  faUsers,
+  faXmark,
+  faBars,
+  faArrowRightFromBracket,
+  faShieldHalved,
+  faCheck
+);
 
 const app = createApp(App);
 
+app.component("font-awesome-icon", FontAwesomeIcon);
 app.use(router);
 app.mount("#app");

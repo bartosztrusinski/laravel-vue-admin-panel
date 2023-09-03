0f6cba5e91a8f1e5c9fd48069742203915156b7a
Bartosz Trusinski <bartosztrusinski@gmail.com>
2023-09-03 19:38:03 +0200

    create layout template

---
+++ resources/js/components/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="min-h-screen bg-gray-200 pb-5">
+    <Sidebar />
+    <div :class="isMenuOpen ? 'max-lg:blur-xl pointer-events-none' : ''">
+      <Header />
+      <main class="lg:ml-[22.5rem] bg-white rounded-lg mx-4 p-4">
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
+<script setup>
+import { inject } from "vue";
+import Sidebar from "./Sidebar.vue";
+import Header from "./Header.vue";
+const isMenuOpen = inject("isMenuOpen");
+</script>

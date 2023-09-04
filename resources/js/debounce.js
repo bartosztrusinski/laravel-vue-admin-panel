2e6cac2d790ce772ddd4dde80718396331e5c96b
Bartosz Trusinski <bartosztrusinski@gmail.com>
2023-09-04 07:50:27 +0200

    create debounce function

---
+++ resources/js/debounce.js
@@ -0,0 +1,14 @@
+function debounce(fn, wait) {
+  let timer;
+  return function (...args) {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    const context = this;
+    timer = setTimeout(() => {
+      fn.apply(context, args);
+    }, wait);
+  };
+}
+
+export default debounce;

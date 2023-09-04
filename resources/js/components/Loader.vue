0ec634914b4fb6bcc06a231028cbeba2c5a8a8db
Bartosz Trusinski <bartosztrusinski@gmail.com>
2023-09-04 07:51:53 +0200

    create loader component

---
+++ resources/js/components/Loader.vue
@@ -0,0 +1,9 @@
+<template>
+  <div
+    class="animate-spin inline-block w-10 h-10 border-[5px] border-current border-t-transparent text-blue-500 rounded-full"
+    role="status"
+    aria-label="loading"
+  >
+    <span class="sr-only">Loading...</span>
+  </div>
+</template>
